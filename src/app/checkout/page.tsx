0fd93f97e724b15197acd12ae9de6e16fd7017ac
Tushar Utane <tusharutane2@gmail.com>
2024-09-24 09:25:51 +0530

    Added Stripe Payment Integration

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,7 +1,32 @@
+"use client";
 import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckOutPage from "@/sections/CheckOutPage";
+import convertToSubcurrency from "@/lib/convertToSubcurrency";
 
 const Checkout = () => {
-	return <div>Checkout</div>;
+	if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
+		throw new Error("Next public stripe key is undefined");
+	}
+
+	const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+	const amount = 230;
+	return (
+		<div className="mt-40 mx-96">
+			{
+				<Elements
+					stripe={stripePromise}
+					options={{
+						mode: "payment",
+						amount: convertToSubcurrency(amount),
+						currency: "usd",
+					}}>
+					<CheckOutPage amount={amount} />
+				</Elements>
+			}
+		</div>
+	);
 };
 
 export default Checkout;
