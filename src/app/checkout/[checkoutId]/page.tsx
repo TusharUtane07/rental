7495cc0a2df888d7d8902f03019ed88ce68bc120
Tushar Utane <tusharutane2@gmail.com>
2024-09-26 10:29:24 +0530

    Stopped payment temporary will be fixing it soon

--- src/app/checkout/[checkoutId]/page.tsx
+++ src/app/checkout/[checkoutId]/page.tsx
@@ -4,6 +4,9 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckOutPage from "@/sections/CheckOutPage";
 import convertToSubcurrency from "@/lib/convertToSubcurrency";
+import toast from "react-hot-toast";
+import axiosInstance from "@/lib/axios";
+import { useRouter } from "next/navigation";
 
 const Checkout = ({ params }: any) => {
 	const bookingId = params.checkoutId;
@@ -14,9 +17,34 @@ const Checkout = ({ params }: any) => {
 
 	const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 	const amount = 230;
+
+	const router = useRouter();
+	const confirmPayment = async () => {
+		try {
+			const response = await axiosInstance.patch(`/api/book-car/${bookingId}`, {
+				status: "confirmed",
+			});
+
+			if (response.data.result) {
+				toast.success("Payment successfull");
+				router.push("/booking-status");
+			} else {
+				toast.error("Error occurred");
+			}
+		} catch (error: any) {
+			toast.error("Error occurred");
+		}
+	};
+
 	return (
 		<div className="mt-40 lg:mx-[400px] mx-3 md:mx-20 md:mb-20">
-			{
+			<div>Facing some issue in payment method, it will be fixed soon</div>
+			<button
+				onClick={confirmPayment}
+				className="bg-gray-600 text-white px-5 py-2 my-3 rounded-md">
+				Confirm Booking without Payment
+			</button>
+			{/* {
 				<Elements
 					stripe={stripePromise}
 					options={{
@@ -26,7 +54,7 @@ const Checkout = ({ params }: any) => {
 					}}>
 					<CheckOutPage amount={amount} bookingId={bookingId} />
 				</Elements>
-			}
+			} */}
 		</div>
 	);
 };
